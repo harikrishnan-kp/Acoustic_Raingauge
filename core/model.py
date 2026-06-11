beb957d41ae04f0ee85c0eb17a519fa54bd48269
Harikrishnan <133218128+harikrishnan-kp@users.noreply.github.com>
2026-06-11 15:55:03 +0530

    to oops

--- core/model.py
+++ core/model.py
@@ -9,64 +9,65 @@ from utils.helper import config
 from utils.dir import get_weights_dir
 
 
-def load_model(weight_file: str) -> any:
-    """
-    Loads deep learning model, build it and loads weights
-    """
-    weight = os.path.join(get_weights_dir, weight_file)
-    model = lstm_withcnn()
-    model.build(input_shape=config["stft_shape"])
-    model.load_weights(weight)
-    return model
+class RainfallEstimator:
+    def __init__(self):
+        self.model = self.load_model(config["infer_model_name"])
 
+    def load_model(self, weight_file: str):
+        """
+        Loads deep learning model, build it and loads weights
+        """
+        weight = os.path.join(get_weights_dir, weight_file)
+        model = lstm_withcnn()
+        model.build(input_shape=config["stft_shape"])
+        model.load_weights(weight)
+        return model
 
-def lstm_withcnn() -> any:
-    """
-    Creates and returns LSTM models with Conv and Dense blocks
-    """
-    model = Sequential()
-    model.add(Input((1025, 2672, 1)))
-    model.add(Conv2D(64, kernel_size=(8, 8), activation="relu"))
-    model.add(MaxPooling2D(pool_size=(8, 8)))
-    model.add(Conv2D(32, kernel_size=(4, 4), activation="relu"))
-    model.add(MaxPooling2D(pool_size=(4, 4)))
-    model.add(Conv2D(16, kernel_size=(2, 2), activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Reshape((1, -1)))
-    model.add(LSTM(20))
-    model.add(Dense(32))
-    model.add(Dense(16))
-    model.add(Dense(1))
-    return model
+    @staticmethod
+    def lstm_withcnn():
+        """
+        Creates and returns LSTM models with Conv and Dense blocks
+        """
+        model = Sequential()
+        model.add(Input((1025, 2672, 1)))
+        model.add(Conv2D(64, kernel_size=(8, 8), activation="relu"))
+        model.add(MaxPooling2D(pool_size=(8, 8)))
+        model.add(Conv2D(32, kernel_size=(4, 4), activation="relu"))
+        model.add(MaxPooling2D(pool_size=(4, 4)))
+        model.add(Conv2D(16, kernel_size=(2, 2), activation="relu"))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Reshape((1, -1)))
+        model.add(LSTM(20))
+        model.add(Dense(32))
+        model.add(Dense(16))
+        model.add(Dense(1))
+        return model
 
+    def combine_audios(self, file_paths: list) -> np.ndarray:
+        """
+        Combines various audio files in their chronological order
+        based on list of audio file paths provided
+        """
+        audio = np.array([])
+        for file_path in file_paths:
+            audio_segment, _ = librosa.load(file_path, sr=int(config["sampling_rate"]))
+            audio = np.append(audio, audio_segment)
+        return audio
 
-def combine_audios(file_paths: list) -> np.ndarray:
-    """
-    Combines various audio files in their chronological order
-    based on list of audio file paths provided
-    """
-    audio = np.array([])
-    for file_path in file_paths:
-        audio_segment, _ = librosa.load(file_path, sr=int(config["sampling_rate"]))
-        audio = np.append(audio, audio_segment)
-    return audio
+    def extract_stft(self, raw_data: np.ndarray) -> np.ndarray:
+        """
+        Calculates and returns STFT on raw input audio provided
+        """
+        Zxx = librosa.stft(raw_data)
+        stft_sample = np.abs(Zxx)
+        return stft_sample[np.newaxis, :, :]
 
-
-def extract_stft(raw_data: np.ndarray) -> np.ndarray:
-    """
-    Calculates and returns STFT on raw input audio provided
-    """
-    Zxx = librosa.stft(raw_data)
-    stft_sample = np.abs(Zxx)
-    return stft_sample[np.newaxis, :, :]
-
-
-def estimate_rainfall(model: any, file_paths: list) -> float:
-    """
-    Computed the models prediction on input data provided
-    """
-    audio = combine_audios(file_paths)
-    audio = audio[: config["seq_len"]]
-    stft_sample = extract_stft(audio)
-    y_pred = model.predict(stft_sample, verbose=0)[0][0]
-    return y_pred
+    def estimate_rainfall(self, file_paths: list) -> float:
+        """
+        Computed the models prediction on input data provided
+        """
+        audio = self.combine_audios(file_paths)
+        audio = audio[: config["seq_len"]]
+        stft_sample = self.extract_stft(audio)
+        y_pred = self.model.predict(stft_sample, verbose=0)[0][0]
+        return y_pred
--- daq_pi.py
+++ daq_pi.py
@@ -1,157 +1,150 @@
 from os import path, listdir
 from datetime import datetime, timedelta
-import subprocess
+import threading
 
 import pandas as pd
 
 from plugins import BatteryMonitor, MoistureSensor
-from core.model import load_model, estimate_rainfall
+from core.model import RainfallEstimator
+from core.davis_logger import DavisRainGauge
 from core.connectivity import send_data
-from utils.helper import time_stamp_fnamer, load_config, delete_files
+from utils.helper import time_stamp_fnamer, delete_files, config
 from utils.logging import initialize_logging, log_time_remaining, write_rain_data_to_csv
 from utils.dir import get_data_dir
+from utils.audio_rec import record_audio
 
 
-def record_audio(file_path, duration, file_format, resolution, sampling_rate):
-    subprocess.call(
-        [
-            "arecord",
-            "-q",
-            "--duration=" + str(duration),
-            "-t",
-            str(file_format),
-            "-f",
-            str(resolution),
-            "-r",
-            sampling_rate,
-            file_path,
-        ]
-    )
+class AcousticRaingauge:
 
+    def __init__(self):
+        self.DB_write_interval = config["DB_writing_interval_min"]
+        self.num_subsamples = config["infer_inetrval_sec"] // config["sample_duration_sec"]
+        self.record_hours = config["record_hours"]
+        self.end_time = datetime.now() + timedelta(hours=self.record_hours)
+        self.moisture_threshold = config["field_deployed"]
+        self.min_threshold = config["min_threshold"]
+        self.moisture_threshold = config["moisture_threshold"]
 
-def main():
-    config = load_config("config.yaml")
-    DB_write_interval = config["DB_writing_interval_min"]
-    num_subsamples = config["infer_inetrval_sec"] // config["sample_duration_sec"]
-    record_hours = config["record_hours"]
-    end_time = datetime.now() + timedelta(hours=record_hours)
-    field_deployed = config["field_deployed"]
-    min_threshold = config["min_threshold"]
-    moisture_threshold = config["moisture_threshold"]
-    infer_model = load_model(config["infer_model_name"])
-    battery = BatteryMonitor()
-    moisture_sensor = MoistureSensor()
-    db_counter, rain = 0, 0
-    locations = []
-    result_data = []
-    data_dir = get_data_dir()
+        self.acoustic_model = RainfallEstimator()
+        self.battery = BatteryMonitor()
+        self.moisture_sensor = MoistureSensor()
 
-    try:
-        if field_deployed:
-            i = 1 # audio sample number
-            while True:
-                dt_now = datetime.now()
-                print(f"Recording sample number {i} on {dt_now}")
-                audio_fname = time_stamp_fnamer(dt_now) + ".wav"
-                location = path.join(data_dir, audio_fname)
-                record_audio(
-                    location,
-                    config["sample_duration_sec"],
-                    config["file_format"],
-                    config["resolution"],
-                    config["sampling_rate"],
-                )
-                locations.append(location)
+    def run(self):
+        db_counter, rain = 0, 0
+        locations = []
+        result_data = []
+        data_dir = get_data_dir()
 
-                if i % num_subsamples == 0: # if (infer_inetrval // wav_duration) no of audio subsamples are collected
-                    mm_hat = estimate_rainfall(infer_model, locations) # estimating rainfall
-                    print("Estimated rainfall: ", mm_hat)
-
-                    files_and_directories = listdir(data_dir)
-                    files_to_delete = [
-                        path.join(data_dir, f)
-                        for f in files_and_directories
-                        if path.isfile(path.join(data_dir, f))
-                    ]
-
-                    delete_files(files_to_delete)
-                    locations.clear()
-
-                    rain += mm_hat
-                    db_counter += 1
-
-                    # reading moisture sensor
-                    moisture = moisture_sensor.get_data()
-
-                    # reading battery parameters
-                    # solar_V, battery_V, solar_I, battery_I = 17.2, 15.2, 1.5, 2.2
-                    solar_V, battery_V, solar_I, battery_I = (battery.get_dataframe())
-
-                    # sending data to DB
-                    if db_counter == DB_write_interval:
-                        if moisture and moisture < moisture_threshold and rain >= min_threshold:
-                            send_data(config, mm_hat, solar_V, battery_V, solar_I, battery_I)
-                        else:
-                            send_data(config, 0.0, solar_V, battery_V, solar_I, battery_I)
-                        rain, db_counter = 0, 0
-                i += 1
-
-        else:
-            logger = initialize_logging(
-                config["audio_log_filename"],
-                datetime.now(),
-                int(record_hours * (3600 / config["sample_duration_sec"])),
-            )
-            for i in range(1, int(record_hours * (3600 / config["sample_duration_sec"])) + 1):
-                dt_now = datetime.now()
-                logger.info(f"Recording sample number {i} on {dt_now}")
-                audio_fname = time_stamp_fnamer(dt_now) + ".wav"
-                location = path.join(data_dir, audio_fname)
-                record_audio(
-                    location,
-                   config["sample_duration_sec"],
-                    config["file_format"],
-                    config["resolution"],
-                    config["sampling_rate"],
-                )
-                locations.append(location)
-
-                if i % num_subsamples == 0: # estimating rainfall
-                    mm_hat = estimate_rainfall(infer_model, locations)
-                    # logger.info("Estimated rainfall: ", mm_hat)
-                    locations.clear()
-                    moisture = moisture_sensor.get_data() # reading moisture sensor
-                    result_data.append(
-                        {
-                            "time_stamp": dt_now,
-                            "rainfall_estimate": mm_hat,
-                            "moisture": moisture,
-                        }
+        try:
+            if self.moisture_threshold:
+                i = 1 # audio sample number
+                while True:
+                    dt_now = datetime.now()
+                    print(f"Recording sample number {i} on {dt_now}")
+                    audio_fname = time_stamp_fnamer(dt_now) + ".wav"
+                    location = path.join(data_dir, audio_fname)
+                    record_audio(
+                        location,
+                        config["sample_duration_sec"],
+                        config["file_format"],
+                        config["resolution"],
+                        config["sampling_rate"],
                     )
-                    write_rain_data_to_csv(result_data, config["rain_log_filename"])
-                    rain += mm_hat
-                    db_counter += 1
+                    locations.append(location)
 
-                    # reading battery parameters
-                    # solar_V, battery_V, solar_I, battery_I = 17.2, 15.2, 1.5, 2.2
-                    solar_V, battery_V, solar_I, battery_I = (battery.get_dataframe())
-                    
-                    # sending data to DB
-                    if db_counter == DB_write_interval:
-                        if moisture and moisture < moisture_threshold and rain >= min_threshold:
-                            send_data(config, mm_hat, solar_V, battery_V, solar_I, battery_I)
+                    if i % self.num_subsamples == 0: # if (infer_inetrval // wav_duration) no of audio subsamples are collected
+                        mm_hat = self.acoustic_model.estimate_rainfall(locations) # estimating rainfall
+                        print("Estimated rainfall: ", mm_hat)
 
-                        else:
-                            send_data(config, 0.0, solar_V, battery_V, solar_I, battery_I)
-                        rain, db_counter = 0, 0
-                log_time_remaining(logger, end_time)
-            logger.info(f"Finished data logging at {datetime.now()}\n")
+                        files_and_directories = listdir(data_dir)
+                        files_to_delete = [
+                            path.join(data_dir, f)
+                            for f in files_and_directories
+                            if path.isfile(path.join(data_dir, f))
+                        ]
 
-    except KeyboardInterrupt:
-        print("Execution interrupted by user")
-    finally:
-        pass
+                        delete_files(files_to_delete)
+                        locations.clear()
+
+                        rain += mm_hat
+                        db_counter += 1
+
+                        # reading moisture sensor
+                        moisture = moisture_sensor.get_data()
+
+                        # reading battery parameters
+                        # solar_V, battery_V, solar_I, battery_I = 17.2, 15.2, 1.5, 2.2
+                        solar_V, battery_V, solar_I, battery_I = (self.battery.get_dataframe())
+
+                        # sending data to DB
+                        if db_counter == self.DB_write_interval:
+                            if moisture and moisture < self.moisture_threshold and rain >= self.min_threshold:
+                                send_data(config, mm_hat, solar_V, battery_V, solar_I, battery_I)
+                            else:
+                                send_data(config, 0.0, solar_V, battery_V, solar_I, battery_I)
+                            rain, db_counter = 0, 0
+                    i += 1
+
+            else:
+                # run mechanical raingauge in new thread
+                mech_raingauge = DavisRainGauge()
+                rain_thread = threading.Thread(target=mech_raingauge.run, daemon=True).start()
+
+                logger = initialize_logging(
+                    config["audio_log_filename"],
+                    datetime.now(),
+                    int(self.record_hours * (3600 / config["sample_duration_sec"])),
+                )
+                for i in range(1, int(self.record_hours * (3600 / config["sample_duration_sec"])) + 1):
+                    dt_now = datetime.now()
+                    logger.info(f"Recording sample number {i} on {dt_now}")
+                    audio_fname = time_stamp_fnamer(dt_now) + ".wav"
+                    location = path.join(data_dir, audio_fname)
+                    record_audio(
+                        location,
+                    config["sample_duration_sec"],
+                        config["file_format"],
+                        config["resolution"],
+                        config["sampling_rate"],
+                    )
+                    locations.append(location)
+
+                    if i % self.num_subsamples == 0: # estimating rainfall
+                        mm_hat = self.acoustic_model.estimate_rainfall(locations)
+                        # logger.info("Estimated rainfall: ", mm_hat)
+                        locations.clear()
+                        moisture = moisture_sensor.get_data() # reading moisture sensor
+                        result_data.append(
+                            {
+                                "time_stamp": dt_now,
+                                "rainfall_estimate": mm_hat,
+                                "moisture": moisture,
+                            }
+                        )
+                        write_rain_data_to_csv(result_data, config["rain_log_filename"])
+                        rain += mm_hat
+                        db_counter += 1
+
+                        # reading battery parameters
+                        # solar_V, battery_V, solar_I, battery_I = 17.2, 15.2, 1.5, 2.2
+                        solar_V, battery_V, solar_I, battery_I = (self.battery.get_dataframe())
+                        
+                        # sending data to DB
+                        if db_counter == self.DB_write_interval:
+                            if moisture and moisture < self.moisture_threshold and rain >= self.min_threshold:
+                                send_data(config, mm_hat, solar_V, battery_V, solar_I, battery_I)
+
+                            else:
+                                send_data(config, 0.0, solar_V, battery_V, solar_I, battery_I)
+                            rain, db_counter = 0, 0
+                    log_time_remaining(logger, self.end_time)
+                logger.info(f"Finished data logging at {datetime.now()}\n")
+
+        except KeyboardInterrupt:
+            print("Execution interrupted by user")
+        finally:
+            pass
 
 
 if __name__ == "__main__":
-    main()
+    AcousticRaingauge.run()
